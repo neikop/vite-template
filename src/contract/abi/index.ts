a9e0b8558807093f6b8815b3eccad530cdd2ca02
Khac Kien <ii.khackien@gmail.com>
2025-06-03 16:26:04 +0700

    Add abi

---
+++ src/contract/abi/index.ts
@@ -0,0 +1,7 @@
+import ERC20 from "./ERC201.json"
+import IOFT from "./IOFT.json"
+import ISC from "./ISC1.json"
+
+export const ERC20Abi = ERC20
+export const IOFTAbi = IOFT
+export const ISCAbi = ISC
