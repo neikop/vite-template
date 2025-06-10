5f7dc54a208dcc7368cfc49c112478a7fcba0277
manhtrv <0xmanhtrv@gmail.com>
2025-06-10 14:29:54 +0700

    chore: update usdc config

--- src/services/devnetService.ts
+++ src/services/devnetService.ts
@@ -12,12 +12,12 @@ const filterData = (items: Token[], params?: TokensParams) => {
 const fetchTokens = (params?: TokensParams): Promise<TokensPagination> => {
   const data: Token[] = [
     {
-      address: "0xb4B575642D2E2bC23307288244507dD903c861d3",
+      address: "0x974BEdB866852dbB57dC7B025af528e51968DF1C",
       chainId: 84004,
       decimals: 18,
       intent: {
-        handler: "0x39D169765CE219D54C94A8B32A7b141179415E05",
-        isc: "0x08e2a0f80a3e98fDa6E014Da2601D7c9F514Df55",
+        handler: "0xc8207E393892ce27F294661D11305DC4a91F10bd",
+        isc: "0x4A0C49396e6ceDBD53f00d8373b4f5E3754Df74a",
       },
       logoURI: "https://s2.coinmarketcap.com/static/img/coins/64x64/3408.png",
       name: "USDC",
